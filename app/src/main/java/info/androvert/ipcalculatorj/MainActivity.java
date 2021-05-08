3050c59b8f68430432464098d5ebbaef5d627569
Alexey Radke <alexeyradke@gmail.com>
2021-05-08 12:20:33 +0700

    Fixed minor bugs related to scrolling, text display. The app is ready for release

--- app/src/main/java/info/androvert/ipcalculatorj/MainActivity.java
+++ app/src/main/java/info/androvert/ipcalculatorj/MainActivity.java
@@ -2,25 +2,36 @@ package info.androvert.ipcalculatorj;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private EditText ipAddressEdit;
     private EditText idEdit;
+    private TextView id;
     private boolean isSwitchChecked;
+    private String stateId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
 
         ipAddressEdit = findViewById(R.id.editTextIp);
         idEdit = findViewById(R.id.editTextId);
+        id = findViewById(R.id.textId);
         isSwitchChecked = false;
 
         Switch switchNetworkAddress = findViewById(R.id.switchNetworkIp);
@@ -40,7 +52,6 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     @SuppressLint("SetTextI18n")
     public void onClick(View view) {
         Spinner mask = findViewById(R.id.spinnerMask);
-        TextView id = findViewById(R.id.textId);
         TextView networkClass = findViewById(R.id.textDecNetworkClass);
         TextView decNetworkIpAddress = findViewById(R.id.textDecNetworkIpAddress);
         TextView subnetNode = findViewById(R.id.textDecSubnetNode);
@@ -89,7 +100,8 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
 
         } catch (Exception e) {
 
-            System.out.println("Exception: " + e);
+            Toast toast = Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT);
+            toast.show();
 
         }
     }
@@ -104,15 +116,13 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         ConstraintLayout view = (ConstraintLayout) getLayoutInflater()
                 .inflate(R.layout.dialog_about, null);
-        builder.setTitle(getString(R.string.about_title))
+        builder
+                .setTitle(getString(R.string.about_title))
                 .setView(view)
+                .setIcon(R.drawable.ipcalculatorlogo)
                 .setCancelable(false)
                 .setNegativeButton(getString(R.string.about_close),
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
+                        (dialog, id) -> dialog.cancel());
         AlertDialog alert = builder.create();
         alert.show();
     }
@@ -121,10 +131,13 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
             ipAddressEdit.setHint(getString(R.string.hint_ip_state_two));
+            stateId = id.getText().toString();
+            id.setText(getString(R.string.text_id));
             idEdit.setVisibility(View.VISIBLE);
             isSwitchChecked = true;
         } else {
             ipAddressEdit.setHint(getString(R.string.hint_ip_state_one));
+            id.setText(stateId);
             idEdit.setVisibility(View.INVISIBLE);
             isSwitchChecked = false;
         }
